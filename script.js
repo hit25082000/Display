50e41dce3273949155416fca316ad3b6e54ba39b
hit25082000 <domingues-pc@hotmail.com>
2024-04-17 14:57:11 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var projetos = [
+  "https://bancositesimples.web.app",
+  "https://templatemedico.web.app/#home",
+  "https://hit25082000.github.io/DevFinancesPP/",
+  "https://hit25082000.github.io/Snake-Js/",
+  "https://hit25082000.github.io/Infortechlado/"
+]
